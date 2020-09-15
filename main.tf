2ecb4747f09d760dfce5622d4c67fe01aa86cde6
qwerty-sudo <64145789+qwerty-sudo@users.noreply.github.com>
2020-09-15 16:50:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,21 +22,3 @@ resource "google_compute_instance" "default" {
     // Apply the firewall rule to allow external IPs to access this instance
     tags = ["http-server"]
 }
-
-resource "google_compute_firewall" "http-server" {
-  name    = "default-allow-http-terraform"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-  // Allow traffic from everywhere to instances with an http-server tag
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["http-server"]
-}
-
-output "ip" {
-  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
-}
